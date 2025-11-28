c83ea72fb0799cc61246d8820da8b86874e0f119
Xoel-Rodriguez-Pereira <xoelrodriguezpereira@gmail.com>
2025-11-28 13:37:02 +0100

    feat: Add find_best_station and greedy_search_global in early state

--- src/find_best_station.py
+++
@@ -1,4 +0,0 @@
-def find_best_station (stations_remaining, covered_states):
-    
-
-    return best_station, best_gradient
--- src/greedy_search_global.py
+++ src/greedy_search_global.py
@@ -1,40 +1,15 @@
-from find_best_station import find_best_station
+stations = {}
 
-def greedy_search_global ():
-
-    stations = {}
-
-    stations["kone"] = set(["id", "nv", "ut"])
-    stations["ktwo"] = set(["wa", "id", "mt"])
-    stations["kthree"] = set(["or", "nv", "ca"])
-    stations["kfour"] = set(["nv", "ut"])
-    stations["kfive"] = set(["ca", "az"])
-    stations["ksix"] = set(["nm", "tx", "ok"])
-    stations["kseven"] = set(["ok", "ks", "co"])
-    stations["keight"] = set(["ks", "co", "ne"])
-    stations["knine"] = set(["ne", "sd", "wy"])
-    stations["kten"] = set(["nd", "ia"])
-    stations["keleven"] = set(["mn", "mo", "ar"])
-    stations["ktwelve"] = set(["la"])
-    stations["kthirteen"] = set(["mo", "ar"])
-
-
-    stations_remaining = stations.copy()
-    covered_states = set()
-    stations_needed = []
-
-    gradients = []
-    num_states_covered = []
-
-    while covered_states < needed_states:
-        best_station, best_gradient = find_best_station(stations_remaining, covered_states)
-
-        if best_station:
-            gradients.append(best_gradient)
-            covered_states |= stations_remaining[best_station]
-            num_states_covered.append(len(covered_states))
-            stations_needed.append(best_station)
-            del stations_remaining[best_station]
-
-    return (stations_needed, num_states_covered, gradients, covered_states)
-
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+stations["ksix"] = set(["nm", "tx", "ok"])
+stations["kseven"] = set(["ok", "ks", "co"])
+stations["keight"] = set(["ks", "co", "ne"])
+stations["knine"] = set(["ne", "sd", "wy"])
+stations["kten"] = set(["nd", "ia"])
+stations["keleven"] = set(["mn", "mo", "ar"])
+stations["ktwelve"] = set(["la"])
+stations["kthirteen"] = set(["mo", "ar"])
